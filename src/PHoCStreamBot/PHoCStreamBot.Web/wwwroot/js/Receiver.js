e5616d2c5755540d400c030995eaa425bd6e913a
Pete Christinzio <peter.christinzio@gmail.com>
2019-07-17 18:32:19 -0400

    added !hi_pete and !yell

--- src/PHoCStreamBot/PHoCStreamBot.Web/wwwroot/js/Receiver.js
+++ src/PHoCStreamBot/PHoCStreamBot.Web/wwwroot/js/Receiver.js
@@ -7,15 +7,34 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/PHoCStreamBotHub")
 
 connection.on("ExecuteCommand", function (command, args) {
 
-    toastr.warning(
-        command.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
-        + " " + args,
-        "COMMAND");
+    if (command.toLowerCase() === 'hi_pete') {
+        $("#hiPete").fadeIn(600).delay(800).fadeOut(600);
+        playAudio('sounds/sup.m4a');
+        return;
+    }
+
+    if (command === 'yell') {
+        $("#bigBanner").text(args);
+        $("#bigBanner").fadeIn(300).delay(700).fadeOut(1000);
+        return;
+    }
+
+    //toastr.warning(
+    //    command.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
+    //    + " " + args,
+    //    "COMMAND");
 });
+
+function playAudio(src) {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', src);
+    audioElement.play();
+}
 
 connection.on("ReceiveMessage", function (message) {
     
     toastr.info(message.text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;"), message.userName);
+
 });
 
 connection.start().then(function () {
