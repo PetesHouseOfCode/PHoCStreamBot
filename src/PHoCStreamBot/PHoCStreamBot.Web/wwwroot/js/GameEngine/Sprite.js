a3ab74ad7b25dc6c4836fa42437335d464cafd97
John Meade <johnnynibbles@gmail.com>
2019-09-13 17:41:32 -0400

    added entity concept to the engine
    used the entity to create a bouncy guy around the screen

--- src/PHoCStreamBot/PHoCStreamBot.Web/wwwroot/js/GameEngine/Sprite.js
+++ src/PHoCStreamBot/PHoCStreamBot.Web/wwwroot/js/GameEngine/Sprite.js
@@ -1,6 +1,8 @@
 ﻿class Sprite {
     currentFrame = 0;
     lastFrameUpdate = 0;
+    xPosition = 0;
+    yPosition = 0;
 
     constructor(frameSheet, fWidth, fHeight, numberFrames, fps) {
         this.frameSheet = frameSheet;
@@ -32,5 +34,42 @@
 
     render(ctx) {
         ctx.drawImage(this.frameSheet, this.currentFrame * this.fWidth, 0, this.fWidth, this.fHeight, this.xPosition, this.yPosition, this.fWidth, this.fHeight);
+    }
+}
+
+class Entity {
+    constructor(sprite, velocity, width, height) {
+        this.sprite = sprite;
+        this.velocity = velocity;
+        this.width = width;
+        this.height = height;
+    }
+
+    update(frame) {
+        this.sprite.update(frame);
+        this.sprite.setPosition(
+            this.sprite.xPosition + (this.velocity.x * (frame.diff/1000)),
+            this.sprite.yPosition + (this.velocity.y * (frame.diff / 1000)));
+
+        if (this.sprite.xPosition > this.width || this.sprite.xPosition < 0) {
+            this.velocity.x = this.velocity.x * -1;
+        }
+
+        if (this.sprite.yPosition > this.height || this.sprite.yPosition < 0) {
+            this.velocity.y = this.velocity.y * -1;
+        }
+    }
+
+    render(ctx) {
+        this.sprite.render(ctx);
+    }
+}
+
+class Vector {
+    x = 0;
+    y = 0;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
 }
--- src/PHoCStreamBot/PHoCStreamBot.Web/wwwroot/js/main-canvas.js
+++ src/PHoCStreamBot/PHoCStreamBot.Web/wwwroot/js/main-canvas.js
@@ -3,6 +3,27 @@
 (function () {
     let canvas = document.getElementById('screen');
 
+    var connection = new signalR.HubConnectionBuilder().withUrl("/PHoCStreamBotHub").build();
+
+    connection.on("ExecuteCommand", function (command, args) {
+        console.debug('Command executed: ' + command);
+
+        if (command.toLowerCase() === 'hi_pete') {
+
+            MyGame.sprites.push(new Text("HI PETE!!!!!!!!", 800, 500));
+            setTimeout(() => { MyGame.sprites.pop(); }, 5000);
+            playAudio('/sounds/sup.m4a');
+            return;
+        }
+
+        if (command === 'yell') {
+
+            MyGame.sprites.push(new Text(args, 800, 400));
+            setTimeout(() => { MyGame.sprites.pop(); }, 5000);
+            return;
+        }
+    });
+
     function main(tFrame) {
         MyGame.stopMain = window.requestAnimationFrame(main);
 
@@ -11,7 +32,8 @@
 
         var ctx = document.getElementById('screen').getContext('2d');
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         update({
             progress: MyGame.progress,
             diff: MyGame.diff
@@ -21,11 +43,19 @@
 
     function update(frame) {
         MyGame.sprites.forEach((sprite) => sprite.update(frame));
-
     }
 
     function render(ctx) {
         MyGame.sprites.forEach((sprite) => sprite.render(ctx));
+    }
+    
+    function playAudio(src) {
+        let media = new Audio(src);
+        const playPromise = media.play();
+        if (playPromise !== null) {
+            playPromise.catch(() => { });
+        }
+        return media;
     }
 
     var image = new Image();
@@ -42,13 +72,25 @@
 
     var adventureImage = new Image();
     adventureImage.src = "/Images/Sprites/adventurer-idle-2.png";
-    let adventureSprite = new Sprite(adventureImage, 100, 74, 4, 6);
+
+    let adventureSprite = new Sprite(adventureImage, 100, 74, 4, 12);
     adventureSprite.setPosition(100, 200);
     MyGame.sprites.push(adventureSprite);
 
+
+    let adventureSprite2 = new Sprite(adventureImage, 100, 74, 4, 4);
+    adventureSprite2.setPosition(100, 280);
+    MyGame.sprites.push(adventureSprite2);
+
     MyGame.sprites.push(new Text("Test Font", 10, 500));
 
-    MyGame.lastTick = performance.now();
-    MyGame.lastRender = MyGame.lastTick;    
-    main(performance.now());
+    let e1 = new Entity(adventureSprite, new Vector(100, -50), canvas.width, canvas.height);
+    MyGame.sprites.push(e1);
+
+    connection.start().then(function () {
+        MyGame.progress = performance.now(); 
+        main(performance.now());
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
 })();
